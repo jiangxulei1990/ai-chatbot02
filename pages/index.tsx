19aa5e63dc52f1c18b619c960698060aaafc6f34
0x_lei <37200602+jiangxulei1990@users.noreply.github.com>
2023-04-19 15:18:56 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
-import { Layout, Text, Page } from '@vercel/examples-ui'
-import { Chat } from '../components/Chat'
+//import { Layout, Text, Page } from '@vercel/examples-ui'
+//import { Chat } from '../components/Chat'
 
 function Home() {
   return (
     <Page className="flex flex-col gap-12">
-      <section className="flex flex-col gap-6">
-       
-       
-      </section>
-
       <section className="flex flex-col gap-3">
         <Text variant="h2">小蚁同学AI助手-Beta</Text>
         <div className="lg:w-2/3">
@@ -19,6 +14,6 @@ function Home() {
   )
 }
 
-Home.Layout = Layout
+//Home.Layout = Layout
 
-export default Home
+//export default Home
